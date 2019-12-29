8684b67751c5abb2cf8e521b6c4e883d7394ff43
Thomas Hisch <t.hisch@gmail.com>
2019-12-29 20:15:27 +0100

    Reemove TODO section from gerrit.el
    I'll create github issues for each of them.

--- gerrit.el
+++ gerrit.el
@@ -35,14 +35,6 @@
 ;;     section local keymap:
 ;;        RET - opens change in browser
 ;; * defun for setting assignee of a gerrit change using rest api `gerrit-rest--set-assignee`
-;;
-;; TODOS:
-;; when uploading a new patchset for a change (via `gerrit-upload`) show votes
-;; include votes in  open gerrit review lines
-;; parse commit messages and show jira tickets (ret on jira tickets opens them)
-;;  where should the jira tickets be displayed?
-;; write some testcases
-;; rename gerrit-upload to gerrit-change-upload and gerrit-download to gerrit-change-download.
 
 ;;; Code:
 
