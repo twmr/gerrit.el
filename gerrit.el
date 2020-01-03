a2350084bf7e264469e5a1805deee8f8394080aa
Thomas Hisch <t.hisch@gmail.com>
2020-01-03 19:59:16 +0100

    Cleanup docstring

--- gerrit.el
+++ gerrit.el
@@ -1,4 +1,4 @@
-;;; gerrit.el --- Gerrit integration -*- lexical-binding: t; -*-
+;;; gerrit.el --- Gerrit client -*- lexical-binding: t; -*-
 
 ;; Author: Thomas Hisch <t.hisch@gmail.com>
 ;; Maintainer: Thomas Hisch <t.hisch@gmail.com>
@@ -24,17 +24,19 @@
 
 ;;; Commentary:
 
-;;
 ;; This package contains
 ;;
-;; * defuns for downloading and uploading a change (`gerrit-upload`, `gerrit-download`)
-;;   The command line tool git-review is used for this under the hood.
-;; * open-reviews section in magit
-;;     The (open) gerrit changes for the current project are queried using the rest API.
+;; * defuns for downloading and uploading a change (`gerrit-upload` and `gerrit-download`)
+;;
+;;   The git-review command line tool as well as the REST API is used for
+;;   these defuns under the hood.
+;;
+;; * open-reviews section for the magit-status buffer (`magit-gerrit-insert-status`)
 ;;
 ;;     section local keymap:
 ;;        RET - opens change in browser
-;; * defun for setting assignee of a gerrit change using rest api `gerrit-rest--set-assignee`
+;;
+;;  See the README.md on the github project page for more information.
 
 ;;; Code:
 
