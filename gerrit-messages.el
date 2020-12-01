4c76ce45ea341daec86fea0aa5c6f6588323643e
Thomas Hisch <thomas.hisch@ims.co.at>
2020-12-01 01:27:21 +0100

    Extend gerrit-messages.el
    Change-Id: Ie44b465019842cdedb9c9042fef0d1511cf665b7

--- gerrit-messages.el
+++ gerrit-messages.el
@@ -1,30 +1,46 @@
-(defun gerrit-section-insert-comments ()
+;; DONE support a filter
+;; TODO use one interactive function for changes and one for topics
+
+(defun gerrit-section--filter (message-info)
+  (or (s-starts-with? "jenkins" (alist-get 'name (alist-get 'author message-info)))
+      (s-ends-with? "/verify" (alist-get 'message message-info))))
+
+
+(defun gerrit-section-insert-comments (topicname)
   (magit-insert-section (toplevel) ; without this, I can't toggle the repo
-                                   ; sections for some reason.
+                                        ; sections for some reason.
 
     (insert "This is some text that describes what this change is about\n")
 
-    (cl-loop for message-info in (gerrit-rest-change-get-messages "Ifa28f572c50a8e6cd894c98bb6ea58babb6c7b0a") do
-             (let* ((name (alist-get 'name (alist-get 'author message-info)))
-                    (date (gerrit--format-abbrev-date (alist-get 'date message-info)))
-                    (msg (s-lines (alist-get 'message message-info)))
-                    (firstline (car msg))
-                    (restlines (s-join "\n" (cdr msg)))
-                    (available-width (- (window-width) (length name) 2))
-                    (section-suffix-fmt (format " %%-%ds %%10s\n" (- available-width 10))))
-               (magit-insert-section (gerrit-comments)
-                 (magit-insert-heading name)
+    (cl-loop for change-info in (gerrit-rest-get-topic-info topicname) do
+             (let ((changenr (alist-get '_number change-info))
+                   (project (alist-get 'project change-info)))
+            (magit-insert-section (change1 changenr)
+              (magit-insert-heading (format "Comments %s @ %s:" changenr project))
+              (cl-loop for message-info in (cl-remove-if #'gerrit-section--filter
+                                                         (gerrit-rest-change-get-messages
+                                                          changenr))
+                       do
+                       (let* ((name (alist-get 'name (alist-get 'author message-info)))
+                              (date (gerrit--format-abbrev-date (alist-get 'date message-info)))
+                              (msg (s-lines (alist-get 'message message-info)))
+                              (firstline (car msg))
+                              (restlines (s-join "\n" (cdr msg)))
+                              (available-width (- (window-width) (length name) 2))
+                              (section-suffix-fmt (format " %%-%ds %%10s\n" (- available-width 10))))
+                         (magit-insert-section (gerrit-comments)
+                           (magit-insert-heading name)
 
-                 ;; copied from magit-insert-child-count
-                 (save-excursion
-                   (goto-char (1- (point)))
-                   (insert (format section-suffix-fmt firstline date))
-                   (delete-char 1)
-                   )
+                           ;; copied from magit-insert-child-count
+                           (save-excursion
+                             (goto-char (1- (point)))
+                             (insert (format section-suffix-fmt firstline date))
+                             (delete-char 1)
+                             )
 
-                 (unless (s-blank? restlines)
-                   (insert (concat restlines "\n")))
-                 )))
+                           (unless (s-blank? restlines)
+                             (insert (concat restlines "\n")))
+                           ))))))
     (insert ?\n)))
 
 (defun gerrit-sec-fmt ()
@@ -33,11 +49,11 @@
          (available-width (- (window-width) (length left) 2)))
     (format (format " %%s %%%ds " available-width) left "10days ago")))
 
-(defun gerrit-section-demo ()
-  (interactive)
+(defun gerrit-section-demo (topicname)
+  (interactive "sEnter topicname: ")
   (let ((buffer (get-buffer-create "*gerrit-section*")))
     (with-current-buffer buffer
       (let ((inhibit-read-only t))
         (erase-buffer)
         (magit-section-mode)
-        (gerrit-section-insert-comments)))))
+        (gerrit-section-insert-comments topicname)))))
