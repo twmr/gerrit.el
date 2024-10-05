0e542524411aa1e60202769203a0437001fcdec7
Aaron Ji <shuxiao9058@gmail.com>
2024-10-05 14:40:23 +0800

    Fix View Change's Patch Chinese display issue

--- gerrit-rest.el
+++ gerrit-rest.el
@@ -442,25 +442,27 @@ to CHANGENR is not locally cloned."
                         gerrit-host
                         gerrit-rest-endpoint-prefix
                         (format "/changes/%s/revisions/current/patch" changenr))))
-    (message "Opening patch of %s" changenr)
-    (setq gerrit-patch-buffer (get-buffer-create "*gerrit-patch*"))
 
+    (message "Opening patch of %s" changenr)
+
+    (setq gerrit-patch-buffer (get-buffer-create "*gerrit-patch*"))
     (with-current-buffer gerrit-patch-buffer
       (let ((inhibit-read-only t))
         (erase-buffer)
-        (url-insert-file-contents target)
-        (base64-decode-region (point-min) (point-max))
-        (delete-trailing-whitespace))
+	(url-insert-file-contents target)
+	(base64-decode-region (point-min) (point-max))
+	(delete-trailing-whitespace)
+	(decode-coding-region (point-min) (point-max) 'utf-8))
+
       (unless buffer-read-only
-        (read-only-mode t))
+	(read-only-mode t))
       ;; using magit-diff-mode would be nice here, but it doesn't work.
       (unless (bound-and-true-p diff-mode)
-        (diff-mode))
+	(diff-mode))
       ;; currently [TAB] is bound to diff-hunk-next. Do we want to change it
       ;; to `outline-cycle'?
       ;; beginning with emacs 28 set outline-minor-mode-cycle to t.
       (outline-minor-mode))
-
     (switch-to-buffer gerrit-patch-buffer)))
 
 
