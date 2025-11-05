2e369786888f4ed040fafcb9cb81c45d6c468662
Thomas Wimmer <thomas.wimmer@ims.co.at>
2025-11-05 22:05:49 +0100

    Remove not-used env var in run-manual-tests.sh
    Change-Id: Id48f71fbdc8f207179345062829f0e0cce495db1

--- run-manual-tests.sh
+++ run-manual-tests.sh
@@ -21,13 +21,6 @@ INIT_PACKAGE_EL="(progn
   (push '(\"melpa\" . \"https://melpa.org/packages/\") package-archives)
   (package-initialize))"
 
-# Byte compile, failing on byte compiler errors, or on warnings unless ignored
-if [ -n "${EMACS_LINT_IGNORE+x}" ]; then
-    ERROR_ON_WARN=nil
-else
-    ERROR_ON_WARN=t
-fi
-
 "$EMACS" -Q -batch \
          --eval "$INIT_PACKAGE_EL" \
          -L . \
